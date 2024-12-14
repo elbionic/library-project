e8678d85e1972d8a88b443c37949d69e75204d49
elbionic <info@elbionic.com>
2024-12-14 12:09:39 +0100

    Add prototype for book objects and some samples

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ const myLibrary =
             read: "not yet"
         }
     ];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        const info = this.title + " by " + this.author + ", " + this.pages
+            + " pages, " + this.read + "!"
+        return info;
+    };
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    let nameBook = new Book(title, author, pages, read);
+    myLibrary.push(nameBook);
+    return myLibrary
+}
+
+addBookToLibrary("Ist da Schnee in meinem Tee?", "Christian Steifen", 123, "reading");
+addBookToLibrary("Ist Gott ein Mathematiker?", "Mario Livio", 366, "read");
