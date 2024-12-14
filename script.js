a0d7d71a018e180ecd2a1d552912542f7964b849
elbionic <info@elbionic.com>
2024-12-14 11:57:30 +0100

    Add the myLibrary Array with two example books

---
+++ script.js
@@ -0,0 +1,15 @@
+const myLibrary =
+    [
+        {
+            title: "Warum juckte mein Auge?",
+            author: "Gott und die Welt",
+            pages: 1,
+            read: "not yet"
+        },
+        {
+            title: "Wie furzt das Nashorn?",
+            author: "Trazan",
+            pages: 20,
+            read: "not yet"
+        }
+    ];
