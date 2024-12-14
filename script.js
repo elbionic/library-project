ad37caf5e97e3bb2af6964dd9a9cc8f69be7cff6
elbionic <info@elbionic.com>
2024-12-14 12:11:53 +0100

    Add displayBooks() function
    The function displays all the books nicely in a
    table. This is an awesome function.

--- script.js
+++ script.js
@@ -34,3 +34,34 @@ function addBookToLibrary(title, author, pages, read) {
 
 addBookToLibrary("Ist da Schnee in meinem Tee?", "Christian Steifen", 123, "reading");
 addBookToLibrary("Ist Gott ein Mathematiker?", "Mario Livio", 366, "read");
+
+const tableBody = document.querySelector('.table-body');
+
+function displayBooks() {
+    myLibrary.forEach(function (book) {
+        const tableRow = document.createElement("tr");
+        tableRow.classList.add("tr-parent");
+        const tableData0 = document.createElement("td");
+        const tableData1 = document.createElement("td");
+        const tableData2 = document.createElement("td");
+        const tableData3 = document.createElement("td");
+        const tableData4 = document.createElement("td");
+        const tableData5 = document.createElement("td");
+        const removeBtn = document.createElement("button");
+        tableData0.textContent = `${book.number}`;
+        tableData1.textContent = `${book.title}`;
+        tableData2.textContent = `${book.author}`;
+        tableData3.textContent = `${book.pages}`;
+        tableData4.textContent = `${book.read}`;
+        removeBtn.textContent = 'remove'
+        tableBody.appendChild(tableRow);
+        tableRow.appendChild(tableData0);
+        tableRow.appendChild(tableData1);
+        tableRow.appendChild(tableData2);
+        tableRow.appendChild(tableData3);
+        tableRow.appendChild(tableData4);
+        tableRow.appendChild(tableData5);
+        tableData5.appendChild(removeBtn);
+        removeBtn.classList.add("removeBtn");
+    });
+};
